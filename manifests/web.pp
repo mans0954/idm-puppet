adf62e0eac1504a67e271e346806d59be5278acb
Alexander Dutton <alexander.dutton@it.ox.ac.uk>
2017-07-18 21:19:20 +0100

    Specify mod_path for py3 mod_wsgi

--- manifests/web.pp
+++ manifests/web.pp
@@ -3,5 +3,8 @@ class idm::web {
     #default_vhost => false
   }
   class { 'apache::mod::ssl': }
-  class { 'apache::mod::wsgi': package_name => "libapache2-mod-wsgi-py3" }
+  class { 'apache::mod::wsgi':
+    package_name => "libapache2-mod-wsgi-py3",
+    mod_path => "wsgi.conf"
+  }
 }
