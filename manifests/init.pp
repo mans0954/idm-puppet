a357428088fe4c57426e0d8d7142e8045e9534fb
Alexander Dutton <alexander.dutton@it.ox.ac.uk>
2017-07-22 21:04:49 +0100

    OIDC tying-together

--- manifests/init.pp
+++ manifests/init.pp
@@ -15,6 +15,8 @@ class idm (
   $realm = hiera('idm::kerberos::realm', 'EXAMPLE.ORG')
   $core_server_name = hiera('idm::core::server_name', "core.${base_domain}")
   $auth_server_name = hiera('idm::auth::server_name', "auth.${base_domain}")
+  $core_oidc_client_id = hiera('idm::core::oidc::client_id')
+  $core_oidc_client_secret = hiera('idm::core::oidc::client_secret')
 
   include postgresql::server
 
@@ -41,6 +43,13 @@ class idm (
       additional_environment => [
         "IDM_AUTH_URL=https://$auth_server_name/",
         "IDM_AUTH_API_URL=https://$auth_server_name/api/",
+        "OIDC_ISSUER=https://$auth_server_name/",
+        "OIDC_AUTHORIZATION_ENDPOINT=https://$auth_server_name/openid/authorize",
+        "OIDC_TOKEN_ENDPOINT=https://$auth_server_name/openid/token",
+        "OIDC_USERINFO_ENDPOINT=https://$auth_server_name/openid/userinfo",
+        "OIDC_JWKS_URL=https://$auth_server_name/openid/jwks",
+        "OIDC_CLIENT_ID=$core_oidc_client_id",
+        "OIDC_CLIENT_SECRET=$core_oidc_client_secret",
       ];
     auth:
       app_package => "idm_auth",
