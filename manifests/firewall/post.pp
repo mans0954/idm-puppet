325d90849cf52ae917115b625c9db15e9c2bd3a4
Alexander Dutton <alexander.dutton@it.ox.ac.uk>
2017-08-04 20:10:41 +0100

    Multiple sources for firewall rules

--- manifests/firewall/post.pp
+++ manifests/firewall/post.pp
@@ -1,15 +1,22 @@
 class idm::firewall::post {
-  firewall {
-    "100 allow SSH":
-      dport => 22,
-      proto => "tcp",
-      action => "accept",
-      source => hiera("firewall::allow_ssh_from");
-    "100 allow HTTP and HTTPS":
-      dport => [80, 443],
-      proto => "tcp",
-      action => "accept",
-      source => hiera("firewall::allow_http_from");
+  hiera("firewall::allow_ssh_from").each |Integer $index, String $source| {
+    firewall {
+      "100 allow SSH %index":
+        dport  => 22,
+        proto  => "tcp",
+        action => "accept",
+        source => $source;
+    }
+  }
+
+  hiera("firewall::allow_http_from").each |Integer $index, String $source| {
+    firewall {
+      "100 allow HTTP and HTTPS":
+        dport  => [80, 443],
+        proto  => "tcp",
+        action => "accept",
+        source => $source;
+    }
   }
 
   firewall { "999 drop all other requests":
