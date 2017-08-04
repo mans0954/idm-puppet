c01dfb5433e9d89853c71fa2679e6220a8ae4704
Alexander Dutton <alexander.dutton@it.ox.ac.uk>
2017-08-04 20:11:36 +0100

    Typo fixing

--- manifests/firewall/post.pp
+++ manifests/firewall/post.pp
@@ -1,7 +1,7 @@
 class idm::firewall::post {
   hiera("firewall::allow_ssh_from").each |Integer $index, String $source| {
     firewall {
-      "100 allow SSH %index":
+      "100 allow SSH $index":
         dport  => 22,
         proto  => "tcp",
         action => "accept",
@@ -11,7 +11,7 @@ class idm::firewall::post {
 
   hiera("firewall::allow_http_from").each |Integer $index, String $source| {
     firewall {
-      "100 allow HTTP and HTTPS":
+      "100 allow HTTP and HTTPS $index":
         dport  => [80, 443],
         proto  => "tcp",
         action => "accept",
